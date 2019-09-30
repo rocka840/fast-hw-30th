9d4504c941b2a6dc67a5d8e847a405dda4ce813a
unknown <RocKa840@ltpemdomain.local>
2019-09-30 09:20:08 +0200

    if statment

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-var x = 4
+var FirstNumber = prompt("Write the first number:");
+var SecondNumber = prompt("Write the second number:");
+
+if (FirstNumber == SecondNumber) {
+    alert("YES, your numbers are equal ");
+}
+
+if (FirstNumber != SecondNumber) {
+    alert("NO, your numbers are not equal");
+}
